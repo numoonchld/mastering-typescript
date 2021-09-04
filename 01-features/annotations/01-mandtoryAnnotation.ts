719711955c0f033c3f42b82b3b19ff5974360350
Raghavendra Saralaya <17598505+numoonchld@users.noreply.github.com>
2021-09-04 19:11:56 +0530

    third mandatory annotation scenario

--- 01-features/annotations/01-mandtoryAnnotation.ts
+++ 01-features/annotations/01-mandtoryAnnotation.ts
@@ -31,3 +31,24 @@ for (let i = 0; i < words2.length; i++) {
     foundWord2 = true;
   }
 }
+
+/* 3. VARIBLE TYPE CANNOT BE AUTO INFERRED */
+
+// consider the following block of code
+let numbers1 = [-10, -1, 12];
+let numberAboveZero1 = false; // Here, `numberAboveZero1` gets type boolean auto assigned due to initialization along with declaration in same line
+
+for (let i = 0; i < numbers1.length; i++) {
+  if (numbers1[i] > 0) {
+    numberAboveZero1 = numbers1[i]; // But, auto assigned type causes error without understanding the intent of the developer
+  }
+}
+
+// to fix the above block of code
+let numbers2 = [-10, -1, 12];
+let numberAboveZero2: boolean | number = false; // Specify multiple types if value type is expected to change dynamically based on some condition
+
+for (let i = 0; i < numbers2.length; i++) {
+  if (numbers2[i] > 0) {
+    numberAboveZero2 = numbers2[i]; // Type Inference doesnt throw error like the previous code block
+  }
